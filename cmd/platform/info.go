eb98842930176addb35e48446330785dddccbbbc
Petar Efnushev <petar.efnushev@gmail.com>
2020-12-17 13:11:19 +0100

    Add platform info command

--- cmd/platform/info.go
+++ cmd/platform/info.go
@@ -1,12 +1,29 @@
 package platform
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/keitaroinc/enabler/cmd/util"
+	"github.com/spf13/cobra"
+	"os/exec"
+)
 
 var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Get info on platform and platform components",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		log := util.NewLogger("INFO", nil)
+		// get platform info
+		kubeContext := cmd.Flag("kube-context").Value
+		command := exec.Command("kubectl",
+			"--context", fmt.Sprintf("kind-%s", kubeContext.String()),
+			"-n", "istio-system",
+			"get", "service", "istio-ingressgateway",
+			"-o", "jsonpath={.status.loadBalancer.ingress[0].ip}")
+		cmdOut, err := command.Output()
+		if err != nil {
+			log.Fatal("Unable to get platform info, exiting...")
+		}
+		log.Infof("Platform can be accessed through the URL: (http://%s)", string(cmdOut))
 	},
 }
